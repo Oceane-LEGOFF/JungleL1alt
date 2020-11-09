16eda6aebf361c84054afd109c7e829d3ae80625
Océane <oceanekegoff@gmail.com>
2020-11-09 14:58:22 +0100

    Predator et Prey

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -1,4 +1,25 @@
 package com.company;
 
 public class Predator {
+    int AgeNow;
+    int AgeMax;
+    static final int AgeAll = 40;
+
+    int NbLifePointsNow;
+    int NbLifePointsMax;
+    static final int NbLifePointsAll = 120;
+
+    Smellling smellling;
+    Vision vision;
+
+
+    public void move() { }
+
+    public void hunt() { }
+
+    public void mate() { }
+
+    public void eat() { }
+
+    public void drink() { }
 }
--- src/com/company/Prey.java
+++ src/com/company/Prey.java
@@ -1,4 +1,14 @@
 package com.company;
 
 public class Prey {
+    int AgeNow;
+    int AgeMax;
+    static final int AgeAll = 30;
+
+    int NbLifePointsNow;
+    int NbLifePointsMax;
+    static final int NbLifePointsAll = 70;
+
+    Smellling smellling;
+    Vision vision;
 }
